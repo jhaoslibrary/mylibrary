74629665f8c771177a1d431a5bcb4a6fa95d5281
JiangHao <18772101289@163.com>
2019-06-11 17:04:49 +0800

    配置打包gradle

--- mylibrary/src/main/java/com/jhao/mylibrary/MyLibraryActivity.java
+++ mylibrary/src/main/java/com/jhao/mylibrary/MyLibraryActivity.java
@@ -1,7 +1,7 @@
-package com.example.mylibrary;
+package com.jhao.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
